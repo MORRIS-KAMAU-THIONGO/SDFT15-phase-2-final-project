5f73a51f18be235eea2a90a3153d18c2bd3d6ccc
M.K.Thiong'o <kamauthiongo58@gmail.com>
2025-10-23 08:37:51 +0300

    update food card

---
+++ food-app/src/components/FoodCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FoodCard = ({ food, addToCart }) => {
+  return (
+    <div className="food-card">
+      <img src={food.image} alt={food.name} />
+      <h3>{food.name}</h3>
+      <p>{food.description}</p>
+      <p>Price: ${food.price}</p>
+      <button onClick={() => addToCart(food)}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default FoodCard;
